78d422731ad8fac5b40a9e50ace91ce9df100475
Tomas DeSosa <TDeSosa@TD-MacAir.local>
2018-10-03 10:23:09 -0600

    this repo is for practice only

---
+++ js/app.js
@@ -0,0 +1,26 @@
+
+btn.onclick = () => {
+    randomizeDraftOrder = (array) => {
+        var currentIndex = array.length, temporaryValue, randomIndex;
+      
+        // While there remain elements to shuffle...
+        while (0 !== currentIndex) {
+      
+          // Pick a remaining element...
+          randomIndex = Math.floor(Math.random() * currentIndex);
+          currentIndex -= 1;
+      
+          // And swap it with the current element.
+          temporaryValue = array[currentIndex];
+          array[currentIndex] = array[randomIndex];
+          array[randomIndex] = temporaryValue;
+        }
+      
+        return array;
+      }
+      
+      // Used like so
+      let leagueMembers = ["AD", "CA", "DW", "IF", "JC", "KD", "RA", "SA", "TC", "TD", "TW", "TS"]
+      draftOrder = randomizeDraftOrder(leagueMembers);
+      console.log(draftOrder);
+};
